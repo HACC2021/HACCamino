9d0cdc8de8ab2ebac1cd069230080737129fdb6c
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-10-28 22:47:03 -1000

    Update Report Form

--- app/imports/ui/components/report /CreateReport.jsx
+++ app/imports/ui/components/report /CreateReport.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Button, Grid } from 'semantic-ui-react';
+import { Container, Button, Form, TextArea } from 'semantic-ui-react';
 import swal from 'sweetalert';
 import { GoogleMap, Marker, InfoWindow, useLoadScript } from '@react-google-maps/api';
 import { reportDefineMethod } from '../../../api/report/ReportCollection.methods';
@@ -7,7 +7,7 @@ import { reportDefineMethod } from '../../../api/report/ReportCollection.methods
 const containerStyle = {
   width: '100%',
   height: '500px',
-  marginTop: '10px',
+  margin: '10px 0px',
 };
 
 const center = {
@@ -18,7 +18,7 @@ const center = {
 const CreateReport = () => {
   // Google Maps
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'secret',
+    googleMapsApiKey: '',
   });
   const [markers, setMarkers] = useState([]);
   const [selected, setSelected] = useState(null);
@@ -47,7 +47,7 @@ const CreateReport = () => {
     definitionData.notes = finalNotes;
     definitionData.lat = markers[0].lat;
     definitionData.lng = markers[0].lng;
-    definitionData.accessKey = '';
+    definitionData.accessKey = 'blank';
     reportDefineMethod.call(definitionData,
     error => {
       if (error) {
@@ -68,44 +68,48 @@ const CreateReport = () => {
   return (
     <Container>
       <h2>Create Report</h2>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={8}>
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Field width={8}>
             <label>Name</label>
             <input placeholder='Name' onChange={ e => setFinalName(e.target.value)}/>
-          </Grid.Column>
-          <Grid.Column width={8}>
+          </Form.Field>
+          <Form.Field width={8}>
             <label>Location</label>
             <input placeholder='Location' onChange={ e => setFinalLocation(e.target.value)}/>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={8}>
+          </Form.Field>
+        </Form.Group>
+        <Form.Group width='equal'>
+          <Form.Field width={8}>
             <label>Animal Characteristics</label>
             <input placeholder='characteristics' onChange={ e => setFinalCharacteristics(e.target.value)}/>
-          </Grid.Column>
-          <Grid.Column width={8}>
+          </Form.Field>
+          <Form.Field width={8}>
             <label>Animal Behavior</label>
             <input placeholder='behavior' onChange={ e => setFinalAnimalBehavior(e.target.value)}/>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={8}>
+          </Form.Field>
+        </Form.Group>
+        <Form.Group width='equal'>
+          <Form.Field width={8}>
             <label>Number Of People Around The Area</label>
             <input placeholder='people' onChange={ e => setFinalPeople(e.target.value)}/>
-          </Grid.Column>
-          <Grid.Column width={8}>
+          </Form.Field>
+          <Form.Field width={8}>
             <label>Phone Number</label>
             <input placeholder='(xxx)' onChange={ e => setFinalPhone(e.target.value)}/>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={16}>
-            <label>Extra Notes</label>
-            <input placeholder='notes' onChange={ e => setFinalNotes(e.target.value)}/>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+          </Form.Field>
+        </Form.Group>
+        <Form.Group>
+          <Form.Field
+          width={16}
+          id='form-textarea-control-opinion'
+          control={TextArea}
+          label='Notes'
+          placeholder='Notes'
+          onChange={ e => setFinalNotes(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
       { isLoaded ?
       <GoogleMap
       mapContainerStyle={containerStyle}
@@ -128,7 +132,7 @@ const CreateReport = () => {
         </InfoWindow>) : null }
       </GoogleMap> : ' '
       }
-      <Button onSubmit={onSubmit}>
+      <Button onClick={onSubmit}>
         Submit
       </Button>
     </Container>
