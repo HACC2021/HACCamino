26dd9afa725dc95aefa2d4d8f72d6d63a312e79b
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-10-30 11:01:58 -1000

    ViewReport useTracker

--- app/imports/ui/pages/report/ViewReport.jsx
+++ app/imports/ui/pages/report/ViewReport.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Meteor } from 'meteor/meteor';
 import { Container, Loader, Card } from 'semantic-ui-react';
-import { withTracker } from 'meteor/react-meteor-data';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Reports } from '../../../api/report/ReportCollection';
 import ReportItem from '../../components/report/ReportItem';
 import Maps from '../../components/report/Maps';
 
-const ViewReport = ({ ready, allReports }) => (
+const ViewReport = () => {
+  const listLoading = useTracker(() => {
+    const handle = Reports.subscribeReportAdmin();
+    return handle.ready();
+  }, []);
+  const allReports = Reports.getCurrentReports();
+  return (
   <Container>
-    { ready ?
+    { listLoading ?
     <div>
       <Maps allReports={allReports} />
       <Card.Group style={{ marginTop: '10px' }}>
@@ -20,18 +24,6 @@ const ViewReport = ({ ready, allReports }) => (
     }
   </Container>
   );
-
-ViewReport.propTypes = {
-  ready: PropTypes.bool.isRequired,
-  allReports: PropTypes.array.isRequired,
 };
 
-export default withTracker(() => {
-  const username = Meteor.user()?.username;
-  const ready = Reports.subscribeReportAdmin().ready() && username !== undefined;
-  const allReports = Reports.getCurrentReports();
-  return {
-    ready,
-    allReports,
-  };
-})(ViewReport);
+export default ViewReport;
