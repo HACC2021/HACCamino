0aa23d734489e1e2c269e4406d717cb52b1eddf6
Michael <mgainey@hawaii.edu>
2021-11-02 19:11:06 -1000

    not working

--- app/imports/ui/pages/PreReport.jsx
+++ app/imports/ui/pages/PreReport.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { Grid, Header } from 'semantic-ui-react';
 
 const PreReport = () => (
-    <Grid>
-        <Grid.Row>
-            <Header>SUP BRRO</Header>
-        </Grid.Row>
-        <Grid.Row>
-            <Header>SUP BRO 2</Header>
-        </Grid.Row>
-    </Grid>
+    <div style={{ paddingLeft: '1rem', paddingRight: '1rem'}}>
+        <Grid>
+            <Grid.Row columns={4}>
+                <Grid.Column>
+                    <Header>SUP BRRO</Header>
+                </Grid.Column>
+                <Grid.Column>
+                    <Header>SUP BRRO</Header>
+                </Grid.Column>
+                <Grid.Column>
+                    <Header>SUP BRRO</Header>
+                </Grid.Column>
+                <Grid.Column>
+                    <Header>SUP BRRO</Header>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Header>SUP BRO 2</Header>
+            </Grid.Row>
+        </Grid>
+    </div>
 );
 
 export default PreReport;
