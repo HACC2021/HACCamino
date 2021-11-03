05194ef6e965575b1bed1f76d577430ca1caa0ee
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-11-02 14:35:38 -1000

    Update ViewReport.jsx

--- app/imports/ui/pages/report/ViewReport.jsx
+++ app/imports/ui/pages/report/ViewReport.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Loader, Card } from 'semantic-ui-react';
+import { Container, Loader, Card, Tab } from 'semantic-ui-react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Reports } from '../../../api/report/ReportCollection';
 import ReportItem from '../../components/report/ReportItem';
@@ -11,13 +11,37 @@ const ViewReport = () => {
     return handle.ready();
   }, []);
   const allReports = Reports.getCurrentReports();
+  const pendingReports = Reports.getPendingReports();
+  const approvedReports = Reports.getApprovedReports();
+  // const [select, setSelected] = useState(pendingReports);
+  const panes = [
+    {
+      menuItem: 'Pending Reports',
+      render: function one() {
+        return (
+        <Tab.Pane attached={false}>
+          {pendingReports.map((report) => <ReportItem report={report} key={report._id} />)}</Tab.Pane>
+        );
+      },
+    },
+    {
+      menuItem: 'Approved Reports',
+      render: function two() {
+        return (
+        <Tab.Pane attached={false}>
+          {approvedReports.map((report) => <ReportItem report={report} key={report._id} />)}
+        </Tab.Pane>
+        );
+      },
+    },
+  ];
   return (
   <Container>
     { listLoading ?
     <div>
       <Maps allReports={allReports} />
       <Card.Group style={{ marginTop: '10px' }}>
-        {allReports.map((report) => <ReportItem report={report} key={report._id} />)}
+        <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
       </Card.Group>
     </div>
     : <Loader>Loading</Loader>
