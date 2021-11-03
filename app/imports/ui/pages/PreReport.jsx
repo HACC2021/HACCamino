75a513164b42fca6b5af38c85f82ac7021dfa958
Michael <mgainey@hawaii.edu>
2021-11-02 18:05:58 -1000

    still not workign

---
+++ app/imports/ui/pages/PreReport.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid, Header } from 'semantic-ui-react';
+
+const PreReport = () => (
+    <Grid>
+        <Grid.Row>
+            <Header>SUP BRRO</Header>
+        </Grid.Row>
+        <Grid.Row>
+            <Header>SUP BRO 2</Header>
+        </Grid.Row>
+    </Grid>
+);
+
+export default PreReport;
