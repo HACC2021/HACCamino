e7b32f0e45d84da03ffbbeb08661784c030c68ba
Michael <mgainey@hawaii.edu>
2021-11-04 12:49:00 -1000

    added PreReportModal #2 still needs link to report page. Copy for #1

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, Image, Header } from 'semantic-ui-react';
+import PreReportModal from '../components/PreReportModal';
 
 /** A simple static component to render some text for the landing page. */
 const Landing = () => {
@@ -7,7 +8,14 @@ const Landing = () => {
     return (
         <Grid textAlign='center' container>
             <Grid.Row>
-                <Header>This is the landing</Header>
+                <Grid.Column>
+                    <Header>This is the landing</Header>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column>
+                    <PreReportModal/>
+                </Grid.Column>
             </Grid.Row>
         </Grid>
     );
