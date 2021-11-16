509b7d47fafa7c7cdb0d83855b063f063f5bf1fd
Jerome Gallego <gallego6@hawaii.edu>
2021-11-16 13:59:15 -1000

    basic layout done

--- app/imports/ui/pages/Dashboard.jsx
+++ app/imports/ui/pages/Dashboard.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 import { Grid, Header } from 'semantic-ui-react';
 
 const Dashboard = () => (
-  <Grid container textAlign='center'>
-    <Grid.Row>
-      <Grid.Column>
-        <Header
-          as="h2"
-          textAlign="center"
-          content={'Dashboard'}
-          subheader={'Page is under construction. Thank you for your patience.'}
-        />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+    <div className={'dash'}>
+      <Grid textAlign='center' divided>
+        <Grid.Row>
+          <Grid.Column width={5} >
+            <Header
+                as="h2"
+                textAlign="center"
+                content={'Active cases'}
+                subheader={'This is where all the cases will be'}
+            />
+          </Grid.Column>
+          <Grid.Column width={11} color={'grey'}>
+            <Header
+                as="h2"
+                textAlign="center"
+                content={'Cases by SubType'}
+                subheader={'Page is under construction. Thank you for your patience.'}
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
 );
 
 export default Dashboard;
