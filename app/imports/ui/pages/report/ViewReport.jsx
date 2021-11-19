828e2e486401cae841a73d2048bb34482bfe7b7b
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-11-18 22:34:33 -1000

    Update ViewReport.jsx

--- app/imports/ui/pages/report/ViewReport.jsx
+++ app/imports/ui/pages/report/ViewReport.jsx
@@ -17,6 +17,7 @@ const ViewReport = () => {
   }, []);
   const [finalAnimal, setFinalAnimal] = useState({ value: 'All', label: 'All' });
   const [finalIsland, setFinalIsland] = useState({ value: 'All', label: 'All' });
+  const [status, setStatus] = useState({ value: 'All', label: 'All' });
   let temp = allReports;
   const animalDropdown = [
     { value: 'All', label: 'All' },
@@ -35,17 +36,27 @@ const ViewReport = () => {
     { value: 'Niʻihau', label: 'Niʻihau' },
     { value: 'Kahoʻolawe', label: 'Kahoʻolawe' },
   ];
+  const approvedDropdown = [
+    { value: 'All', label: 'All' },
+    { value: 'pending', label: 'pending' },
+    { value: 'approved', label: 'approved' },
+  ];
     if (finalAnimal.value !== 'All') {
-      temp = allReports.filter((report) => report.animal === finalAnimal.value);
-      let second = temp;
+      let second = allReports.filter((report) => report.animal === finalAnimal.value);
       if (finalIsland.value !== 'All') {
-        second = temp.filter((report) => report.island === finalIsland.value);
+        second = second.filter((report) => report.island === finalIsland.value);
+      }
+      if (status.value !== 'All') {
+        second = second.filter((report) => report.status === status.value);
       }
       temp = second;
     } else {
       let second = allReports;
       if (finalIsland.value !== 'All') {
-        second = temp.filter((report) => report.island === finalIsland.value);
+        second = second.filter((report) => report.island === finalIsland.value);
+      }
+      if (status.value !== 'All') {
+        second = second.filter((report) => report.status === status.value);
       }
       temp = second;
     }
@@ -55,7 +66,7 @@ const ViewReport = () => {
     <div>
       <Form>
         <Form.Group widths='equal'>
-          <Form.Field width={8} required>
+          <Form.Field width={4} required>
             <label>Type Of Animal</label>
             <Select
             options={animalDropdown}
@@ -64,13 +75,22 @@ const ViewReport = () => {
             defaultValue={finalAnimal}
             />
           </Form.Field>
-          <Form.Field width={8} required>
+          <Form.Field width={4} required>
             <label>Island</label>
             <Select
             options={islandDropdown}
             name='island'
             onChange={setFinalIsland}
             defaultValue={finalIsland}
+            />
+          </Form.Field>
+          <Form.Field width={4} required>
+            <label>Status</label>
+            <Select
+            options={approvedDropdown}
+            name='status'
+            onChange={setStatus}
+            defaultValue={status}
             />
           </Form.Field>
         </Form.Group>
