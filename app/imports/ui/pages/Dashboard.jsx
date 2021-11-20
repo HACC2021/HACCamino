51fb46bd8933f81cc982065cde90c04e51282684
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-11-19 15:20:31 -1000

    Checkpoint

--- app/imports/ui/pages/Dashboard.jsx
+++ app/imports/ui/pages/Dashboard.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Tab } from 'semantic-ui-react';
 import CaseGraph from '../components/CaseGraph';
 import AdminListReports from '../components/AdminListReports';
 import Export from './Export';
 
+const panes = [
+  { menuItem: 'List of Reports', render: () => <Grid textAlign='center'>
+      <Grid.Row>
+      <AdminListReports/>
+    </Grid.Row>
+      <Grid.Row>
+        <Export/>
+      </Grid.Row> </Grid> },
+  { menuItem: 'Report Analytics', render: () => <Grid textAlign='center'>
+      <Grid.Row>
+        <Grid.Column>
+          <h2>Report Analytics</h2>
+          <CaseGraph/>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid> },
+];
+
 const Dashboard = () => (
     <div className={'dash'}>
-      <Grid textAlign='center'>
-        <Grid.Row>
-          <AdminListReports/>
-        </Grid.Row>
-        <Grid.Row>
-          <Export/>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <h2>Report Analytics</h2>
-            <CaseGraph/>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <Tab panes={panes} />
     </div>
 );
 
